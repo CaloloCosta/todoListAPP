fac6d50b33a31eee39ec22d59f75ec87ab2d74f8
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-02-07 14:54:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
-let input = document.querySelector('input');
-let ul = document.querySelector('ul');
-let form = document.querySelector('form');
-let clear = document.getElementById('clear');
-let tasksBoard = document.querySelector('.tasksBoard');
-let counter=0;
-// add task to the list
-form.addEventListener('submit',()=>{
+getLocalStorage();
+loadEvents();
+// load every event in the page
+function loadEvents(){
+  document.querySelector('form').addEventListener('submit',submit);
+  document.getElementById('clear').addEventListener('click',clearList);
+  document.querySelector('ul').addEventListener('click',deleteTask);
+}
+
+// subit data function
+function submit(e){
+  e.preventDefault();
+  let taskList;
+  let input = document.querySelector('input');
+  // checks if there is any data saved
+  if(localStorage.getItem('tasks') == null){
+    taskList = [];
+  }
+  else{
+    taskList = JSON.parse(localStorage.getItem('tasks'));
+  }
+  taskList.push(input.value);
+  saveLocally(taskList);
   addTask(input.value);
   input.value = '';
-});
+
+}
+// add tasks
 function addTask(task){
-  event.preventDefault();
+  let ul = document.querySelector('ul');
   let li = document.createElement('li');
-  li.innerHTML = '<span class="delete">×</span><input type="checkbox">'+task;
-  tasksBoard.style.display = 'block';
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox">${task}`;
   ul.appendChild(li);
-  let spans = document.getElementsByTagName('span');
-  let check = document.querySelectorAll('input[type="checkbox"]');
-  checkedTask(check);
-  deleteTodo(spans);
-
-
+  document.querySelector('.tasksBoard').style.display = 'block';
 }
+// save in localStorage
+function saveLocally(task){
+  localStorage.setItem('tasks',JSON.stringify(task));
+}
+// get data from localStorage
+function getLocalStorage(){
+  let taskList;
+  // checks if there is any data saved
+  if(localStorage.getItem('tasks') == null){
+    taskList = [];
+  }
+  else{
+    taskList = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  if(taskList.length > 0){
+    taskList.forEach(function(task){
+      addTask(task);
+    });
+  }
+}
+
 // clear the LIST
-clear.addEventListener('click',clearList);
-function clearList(){
-  ul.innerHTML = '';
-  tasksBoard.style.display = 'none';
+function clearList(e){
+  let ul = document.querySelector('ul').innerHTML = '';
+  // clear local Storage data
+  localStorage.clear();
 }
 
-// remove
-function deleteTodo(t){
-  for(let i = 0; i < t.length; i++){
-    t[i].addEventListener('click',()=>{
-      t[i].parentNode.style.display = 'none';
-    });
+// delete task
+function deleteTask(e){
+  if(e.target.className = 'delete'){
+    let remove = e.target.parentNode;
+    let text = e.target.parentNode.lastChild.textContent;
+    let parentNode = remove.parentNode;
+    parentNode.removeChild(remove);
+    // delete from local Storage
+    removeFromLocalStorage(text);
   }
 
 }
-// check a tasks
-function checkedTask(check){
-  for(let i = 0; i < check.length; i++){
-    check[i].addEventListener('change',()=>{
-      if(check[i].checked){
-        check[i].parentNode.style.color = '#c2c4c3';
-        check[i].parentNode.style.textDecoration = 'line-through';
-      }else{
-        check[i].parentNode.style.color = '#0f222d';
-        check[i].parentNode.style.textDecoration = 'none';
-      }
-
-    });
+// remove from local Strorage
+function removeFromLocalStorage(task){
+  let taskList;
+  // checks if there is any data saved
+  if(localStorage.getItem('tasks') == null){
+    taskList = [];
   }
+  else{
+    taskList = JSON.parse(localStorage.getItem('tasks'));
+  }
+  taskList.splice(taskList.indexOf(task),1);
+  localStorage.setItem('tasks',JSON.stringify(taskList));
 }
