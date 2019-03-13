3ecc57f6f339ef8362bc7b80bb18a52f0ea50b4b
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-03-14 00:43:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,41 @@
-getLocalStorage();
 loadEvents();
 // load every event in the page
 function loadEvents(){
   document.querySelector('form').addEventListener('submit',submit);
   document.getElementById('clear').addEventListener('click',clearList);
-  document.querySelector('ul').addEventListener('click',deleteTask);
-}
+  document.querySelector('ul').addEventListener('click',deleteTick);
 
+}
 // subit data function
 function submit(e){
   e.preventDefault();
   let taskList;
   let input = document.querySelector('input');
-  // checks if there is any data saved
-  if(localStorage.getItem('tasks') == null){
-    taskList = [];
-  }
-  else{
-    taskList = JSON.parse(localStorage.getItem('tasks'));
-  }
-  taskList.push(input.value);
-  saveLocally(taskList);
   addTask(input.value);
   input.value = '';
-
 }
+
 // add tasks
 function addTask(task){
   let ul = document.querySelector('ul');
   let li = document.createElement('li');
-  li.innerHTML = `<span class="delete">×</span><input type="checkbox">${task}`;
+  li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
   ul.appendChild(li);
   document.querySelector('.tasksBoard').style.display = 'block';
-}
-// save in localStorage
-function saveLocally(task){
-  localStorage.setItem('tasks',JSON.stringify(task));
-}
-// get data from localStorage
-function getLocalStorage(){
-  let taskList;
-  // checks if there is any data saved
-  if(localStorage.getItem('tasks') == null){
-    taskList = [];
-  }
-  else{
-    taskList = JSON.parse(localStorage.getItem('tasks'));
-  }
-
-  if(taskList.length > 0){
-    taskList.forEach(function(task){
-      addTask(task);
-    });
-  }
 }
 
 // clear the LIST
 function clearList(e){
   let ul = document.querySelector('ul').innerHTML = '';
-  // clear local Storage data
-  localStorage.clear();
+}
+
+// deleteTick
+function deleteTick(e){
+  if(e.target.className == 'delete')
+    deleteTask(e);
+  else {
+    tickTask(e);
+  }
 }
 
 // delete task
@@ -69,21 +45,17 @@ function deleteTask(e){
     let text = e.target.parentNode.lastChild.textContent;
     let parentNode = remove.parentNode;
     parentNode.removeChild(remove);
-    // delete from local Storage
-    removeFromLocalStorage(text);
   }
-
 }
-// remove from local Strorage
-function removeFromLocalStorage(task){
-  let taskList;
-  // checks if there is any data saved
-  if(localStorage.getItem('tasks') == null){
-    taskList = [];
+
+// tick a task
+function tickTask(e){
+  const task = e.target.nextSibling;
+  if(e.target.checked){
+    task.style.textDecoration = "line-through";
+    task.style.color = "#ff0000";
+  }else {
+    task.style.textDecoration = "none";
+    task.style.color = "#2f4f4f";
   }
-  else{
-    taskList = JSON.parse(localStorage.getItem('tasks'));
-  }
-  taskList.splice(taskList.indexOf(task),1);
-  localStorage.setItem('tasks',JSON.stringify(taskList));
 }
